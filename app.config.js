568a09cf9d0d1809a31f12bf1c2fd62e70a080c2
Jefferson Solano <jefferson.solano@cam.org.au>
2024-06-07 22:25:11 +1000

    update version android

--- app.config.js
+++ app.config.js
@@ -74,7 +74,7 @@ module.exports = {
         "android.permission.ACCESS_FINE_LOCATION",
         "android.permission.FOREGROUND_SERVICE",
       ],
-      versionCode: 59,
+      versionCode: 1,
       googleServicesFile: "./GoogleAnalytics/google-services.json"
     },
     description: "Coded by Digital Basis",
