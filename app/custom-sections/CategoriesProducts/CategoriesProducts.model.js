a2ab40df04a68b8b21052e9296a5856a2b80c9bd
Jefferson Solano <jefferson.solano@cam.org.au>
2024-06-18 08:36:04 +1000

    update word

--- app/custom-sections/CategoriesProducts/CategoriesProducts.model.js
+++ app/custom-sections/CategoriesProducts/CategoriesProducts.model.js
@@ -2,7 +2,7 @@ import { images } from '@assets/imgs/categories'
 
 export const cardInfo = {
   id: '',
-  name: 'All products',
+  name: 'All Products',
   image: { uri: 'https://5145345.app.netsuite.com/c.5145345/site/Homepage%20Images/swanonline%20Main%20Banner.png' },
 }
 
