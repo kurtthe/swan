4d472952b66d2bb25941da4ac6c7cc7d749619de
jopatino <jopatino86@misena.edu.co>
2024-06-24 20:28:45 -0500

    Fix searcj button 2

--- app/screens/InvoiceDetail/InvoiceDetail.jsx
+++ app/screens/InvoiceDetail/InvoiceDetail.jsx
@@ -244,12 +244,6 @@ export const InvoiceDetails = ({ route }) => {
               <Text>{invoiceDetail.storeLocation === null ? "" : validateEmptyField(invoiceDetail.storeLocation.name)}</Text>
             </Block>
           </Block>
-          <Block row>
-            <Block flex>
-              <Text style={styles.text}>{invoiceDetail?.type} Description</Text>
-              <Text>{validateEmptyField(invoiceDetail.description)}</Text>
-            </Block>
-          </Block>
         </Block>
         <Block card style={styles.content}
         >
