2fb50f7cbe76e901555a4d728ec18f056bf1f3af
Mateo Nieto <matheo.developer@gmail.com>
2024-06-28 19:22:18 -0500

    list invoices to type

--- app/components/CardPromotion.tsx
+++ app/components/CardPromotion.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import {View, Image, Text, StyleSheet} from 'react-native'
 import { imagePromotion } from '@assets/imgs/prom';
 
-
-type Props = {};
-
-const CardPromotion:React.FC<Props> = ({}) => {
+const CardPromotion = ({}) => {
 
   return (
     <View>
@@ -16,11 +13,19 @@ const CardPromotion:React.FC<Props> = ({}) => {
         <Text>
           Purchase any eligible model via the Swan Plumbing Portal during the purchase period and you will go into the draw to win a Garmin Forerunner® 55 Smart Watch (valued at RRP $349).
         </Text>
+        <Text>
+          *Purchase period: 1 June to 31 August 2024
+        </Text>
 
+        <Text>
+          Learn more
+        </Text>
       </View>
     </View>
   )
 
 }
+
+const styles = StyleSheet.create({})
 
 export default CardPromotion
