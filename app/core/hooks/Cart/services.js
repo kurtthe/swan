c27ce75b814f2b6b837ad2a82fd68e1b2dbb2560
Mateo Nieto <matheo.developer@gmail.com>
2025-02-12 18:48:11 +0000

    changes from tradeTrak

--- app/core/hooks/Cart/services.js
+++ app/core/hooks/Cart/services.js
@@ -41,6 +41,7 @@ export const getOrdersService = async page => {
       },
     },
   );
+  console.log('===>supplierId::', supplierId);
   const totalPages =
     parseInt(responseOrders.headers['x-pagination-page-count']) || 2;
   const dataToResponse = {
--- app/custom-sections/ListCart/ListCart.jsx
+++ app/custom-sections/ListCart/ListCart.jsx
@@ -1,50 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList } from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {FlatList} from 'react-native';
 import ProductCart from '@custom-elements/ProductCart';
-import { Block, Text, Button } from 'galio-framework';
-import { nowTheme } from '@constants/index';
+import {Block, Text, Button} from 'galio-framework';
+import {nowTheme} from '@constants/index';
 
-import { makeStyles } from './ListCart.styles'
-import { useSelector } from 'react-redux';
+import {makeStyles} from './ListCart.styles';
+import {useSelector} from 'react-redux';
 import EmptyCart from '@custom-elements/EmptyCart';
-import { ProductCart as ProductCartService } from '@core/services/product-cart.service';
+import {ProductCart as ProductCartService} from '@core/services/product-cart.service';
 
-export const ListCart = ({ onCheckoutPressed, orderTotal, bought, products }) => {
-  const styles = makeStyles()
-  const [valueTotal, setValueTotal] = useState('0')
+export const ListCart = ({onCheckoutPressed, bought, products}) => {
+  const styles = makeStyles();
+  const [valueTotal, setValueTotal] = useState('0');
 
-  const cartProducts = useSelector((state) => state.productsReducer.products);
-  const clientFriendly = useSelector((state) => state.productsReducer.clientFriendly);
+  const cartProducts = useSelector(state => state.productsReducer.products);
+  const clientFriendly = useSelector(
+    state => state.productsReducer.clientFriendly,
+  );
 
-  useEffect(()=>{
+  useEffect(() => {
     const orderTotal = () => {
       const productCartService = ProductCartService.getInstance(cartProducts);
       const total = productCartService.totalOrder();
-      setValueTotal(total)
+      setValueTotal(total);
     };
-    orderTotal()
-  },[cartProducts])
+    orderTotal();
+  }, [cartProducts]);
 
-  const renderProducts = ({ item }) => <ProductCart product={item} bought={bought} />;
-
+  const renderProducts = ({item}) => (
+    <ProductCart product={item} bought={bought} />
+  );
 
   const renderFooter = () => {
     if (!cartProducts || cartProducts.length === 0) {
       return null;
     }
-    const titleOrder = clientFriendly ? 'Total RRP (ex-GST) ' : 'Total (ex-GST)';
+    const titleOrder = clientFriendly
+      ? 'Total RRP (ex-GST) '
+      : 'Total (ex-GST)';
 
     return (
       <Block row style={styles.detailOrders}>
         {!bought && (
           <>
-            <Text style={{ fontWeight: 'bold' }}>{`${titleOrder}: ${valueTotal}`}</Text>
+            <Text
+              style={{
+                fontWeight: 'bold',
+              }}>{`${titleOrder}: ${valueTotal}`}</Text>
             <Button
               shadowless
-              style={{ left: 10 }}
+              style={{left: 10}}
               color={nowTheme.COLORS.INFO}
-              onPress={() => onCheckoutPressed && onCheckoutPressed()}
-            >
+              onPress={() => onCheckoutPressed && onCheckoutPressed()}>
               <Text size={18} color={nowTheme.COLORS.WHITE}>
                 Checkout
               </Text>
@@ -66,6 +73,5 @@ export const ListCart = ({ onCheckoutPressed, orderTotal, bought, products }) =>
       />
       {renderFooter()}
     </>
-  )
-}
-
+  );
+};
